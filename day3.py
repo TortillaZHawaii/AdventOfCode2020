52981273ea19b84500662b8d20f5af5cae129591
TortillaZHawaii <dwysocki157@gmail.com>
2020-12-05 19:45:00 +0100

    added day3 part 2

--- day3.py
+++ day3.py
@@ -2,17 +2,29 @@ import sys
 
 forest = sys.stdin.read().splitlines()
 
-no_trees = 0
-
 repeating_width = len(forest[0]) 
 
-x = 3
-y = 1
 
-while y < len(forest):
-    if forest[y][x] == '#':
-        no_trees += 1
-    x = (x + 3) % repeating_width
-    y += 1
+def no_trees_on_path(path)->int:
+    no_trees = 0
 
-print(no_trees)
+    x = path[0]
+    y = path[1]
+
+    while y < len(forest):
+        if forest[y][x] == '#':
+            no_trees += 1
+        x = (x + path[0]) % repeating_width
+        y += path[1]
+
+    return no_trees
+
+
+paths = [[1,1], [3,1], [5,1], [7,1], [1,2]]
+
+answer = 1
+
+for slope in paths:
+    answer *= no_trees_on_path(slope)
+
+print(answer)
